b86c1194b911a93816f849a23ab6017a7fd136b4
Adam Risberg <risbergadam@gmail.com>
2022-12-02 09:25:35 +0100

    Golf it.

--- src/main/kotlin/aoc2022/aoc2022.kt
+++ src/main/kotlin/aoc2022/aoc2022.kt
@@ -8,13 +8,12 @@ fun main(args: Array<String>) {
 }
 
 fun day1() = File("input/day01.txt").readText()
-    .split("\r\n\r\n")
+    .split("\r\n\r\n") // Thanks windows
     .map { it.trim().split("\r\n").sumOf(String::toInt) }
-    .sorted().reversed()
-    .let { Pair(it[0], it.take(3).sum()) }
+    .sorted().let { Pair(it.last(), it.takeLast(3).sum()) }
 
 fun day2() = File("input/day02.txt").readLines()
-    .map { it.split(" ").let { (a, b)-> Pair(a[0] - 'A', b[0] - 'X') } }
+    .map { Pair(it[0] - 'A', it[2] - 'X') }
     .let { Pair(
         it.sumOf { (a, b) -> b + 1 + (b - a + 1).mod(3) * 3 },
         it.sumOf { (a, b) -> 1 + (a - 1).mod(3) + b * 3 }
