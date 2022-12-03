c9a180ef663324307f4d8357b198190730ccfd11
Adam Risberg <risbergadam@gmail.com>
2022-12-03 09:37:29 +0100

    Day 3.

--- src/main/kotlin/aoc2022/aoc2022.kt
+++ src/main/kotlin/aoc2022/aoc2022.kt
@@ -4,17 +4,33 @@ import java.io.File
 
 fun main(args: Array<String>) {
     // println(day1())
-    println(day2())
+    // println(day2())
+    println(day3())
 }
 
 fun day1() = File("input/day01.txt").readText()
     .split("\r\n\r\n") // Thanks windows
-    .map { it.trim().split("\r\n").sumOf(String::toInt) }
+    .map { it.split("\r\n").sumOf(String::toInt) }
     .sorted().let { Pair(it.last(), it.takeLast(3).sum()) }
 
 fun day2() = File("input/day02.txt").readLines()
     .map { Pair(it[0] - 'A', it[2] - 'X') }
-    .let { Pair(
-        it.sumOf { (a, b) -> b + 1 + (b - a + 1).mod(3) * 3 },
-        it.sumOf { (a, b) -> 1 + (b + a - 1).mod(3) + b * 3 }
-    )}
+    .let {
+        Pair(
+            it.sumOf { (a, b) -> (b - a + 1).mod(3) * 3 + b + 1 },
+            it.sumOf { (a, b) -> (b + a - 1).mod(3) + b * 3 + 1 }
+        )
+    }
+
+fun day3() = File("input/day03.txt").readLines().let { list ->
+    fun prio(c: Char) = if (c >= 'a') (c - 'a' + 1) else (c - 'A' + 27)
+    Pair(
+        list
+            .map { it.take(it.length / 2).toSet() intersect it.drop(it.length / 2).toSet() }
+            .sumOf { prio(it.first()) },
+        list
+            .windowed(3, 3)
+            .map { (a, b, c) -> a.toSet() intersect b.toSet() intersect c.toSet() }
+            .sumOf { prio(it.first()) }
+    )
+}
