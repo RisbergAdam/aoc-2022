2122984c62fd86787a427d68b2b0580143fe808b
Adam Risberg <risbergadam@gmail.com>
2022-12-07 12:40:23 +0100

    Day 7.

--- src/main/kotlin/aoc2022/aoc2022.kt
+++ src/main/kotlin/aoc2022/aoc2022.kt
@@ -1,7 +1,9 @@
 package aoc2022
 
 import java.io.File
-import kotlin.collections.ArrayDeque
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 fun main(args: Array<String>) {
     // println(day1())
@@ -9,7 +11,8 @@ fun main(args: Array<String>) {
     // println(day3())
     // println(day4())
     // println(day5())
-    println(day6())
+    // println(day6())
+    println(day7())
 }
 
 fun day1() = File("input/day01.txt").readText()
@@ -49,8 +52,8 @@ fun day4() = File("input/day04.txt").readLines()
     }
 
 fun day5() = File("input/day05.txt").readText().let { text ->
-    val boxes1 = Array(9) { ArrayDeque<String>() }
-    val boxes2 = Array(9) { ArrayDeque<String>() }
+    val boxes1 = Array(9) { ArrayList<String>() }
+    val boxes2 = Array(9) { ArrayList<String>() }
 
     text.split("\n").forEach { line ->
         Regex("[A-Z]").findAll(line).forEach { match ->
@@ -62,8 +65,8 @@ fun day5() = File("input/day05.txt").readText().let { text ->
     Regex("move (\\d+) from (\\d+) to (\\d+)").findAll(text).forEach { match ->
         val (n, from, to) = match.destructured.toList().map { it.toInt() - 1 }
         (0..n).forEach { i ->
-            boxes1[to].addFirst(boxes1[from].removeAt(0))
-            boxes2[to].addFirst(boxes2[from].removeAt(n - i))
+            boxes1[to].add(0, boxes1[from].removeAt(0))
+            boxes2[to].add(0, boxes2[from].removeAt(n - i))
         }
     }
 
@@ -79,3 +82,37 @@ fun day6() = File("input/day06.txt").readText().let { line ->
         line.windowed(14).indexOfFirst { it.toSet().size == 14 } + 14,
     )
 }
+
+fun day7() = File("input/day07.txt")
+    .readLines()
+    .map { line -> line.split(" ") }
+    .let { lines -> // enrich each line with its pwd
+        lines.scan(listOf<String>()) { pwd, line ->
+            val (t, cmd, arg) = line + "ARG"
+            when {
+                t == "$" && cmd == "cd" && arg == ".." -> pwd.dropLast(1)
+                t == "$" && cmd == "cd" -> pwd + arg
+                else -> pwd
+            }
+        }.zip(lines)
+    }
+    .filter { (_, line) -> line[0] != "$" }
+    .groupBy({ (pwd, _) -> pwd }, { (_, line) ->
+        when {
+            line[0] == "dir" -> 0
+            else -> line[0].toInt()
+        }
+    })
+    .let { directories -> // find the size of each directory in n^2 :')
+        directories.map { (pwd1) ->
+            directories
+                .filter { (pwd2) -> pwd1 == pwd2.take(pwd1.size) }
+                .toList().sumOf { (_, size2) -> size2.sum() }
+        }
+    }.let { dirSizes ->
+        val free = 70000000 - dirSizes.maxOf { it }
+        Pair(
+            dirSizes.filter { it <= 100000 }.sum(),
+            dirSizes.filter { free + it > 30000000 }.minOf { it }
+        )
+    }
